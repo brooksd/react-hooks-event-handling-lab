affe0e947370c6510904b6e2afe6c35fc04d79ff
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-16 17:51:30 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
+import React from "react";
 // Code EyesOnMe Component Here
+const setFocus = () => console.log("Good!");
+const setblur = () => console.log("Hey! Eyes on me!");
+
+const EyesOnMe = () => <button onFocus={setFocus} onBlur={setblur}>Eyes on me</button>
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
+import React from "react";
 // Code Keypad Component Here
+
+
+const Keypad = () => {
+    const change = () => console.log("Entering password...")
+
+    return <input onChange={change} type="password" />
+
+}
+
+export default Keypad;
